b081930e03ac065dc58b73d5d6afc806047be736
Anant2002 <90477164+Anant2002@users.noreply.github.com>
2022-01-03 22:48:22 +0530

    Create InsertionAtEnd.c

---
+++ Arrays/InsertionAtEnd.c
@@ -0,0 +1,6 @@
+#You are given an array arr. The size of the array is given by sizeOfArray. You need to insert an element at the end.
+
+void insertAtEnd(int arr[],int sizeOfArray,int element)
+{
+    arr[sizeOfArray-1]=element;
+}
